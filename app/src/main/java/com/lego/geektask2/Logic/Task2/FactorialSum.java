8ca5b10ad480885a51dcde162d5396aabe871fee
Legementario <mr.re66it@gmail.com>
2016-08-07 12:27:31 +0300

    init commit

---
+++ app/src/main/java/com/lego/geektask2/Logic/Task2/FactorialSum.java
@@ -0,0 +1,23 @@
+package com.lego.geektask2.Logic.Task2;
+
+
+/**
+ * @author Lego on 26.07.2016.
+ */
+public class FactorialSum {
+
+    public void start(){
+        long time = System.nanoTime();
+        StringBuilder tmp = new StringBuilder(MyBigInteger.factorial(100).toDecimalString()); //find the required number and converts number to array of numerals
+        System.out.println("Factorial - "+tmp.toString());
+        int result = 0;
+        for (int i = 0; i < tmp.length(); i++) {
+            result+= Integer.parseInt(tmp.substring(i,i+1));    // sum up all the numbers of array
+        }
+
+        System.out.println("Factorial sum - "+result);
+        System.out.println("\n Time of execution -" + (System.nanoTime() - time) * (Math.pow(10, -9)) + " \n");
+    }
+
+
+}
